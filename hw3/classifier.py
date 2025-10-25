8277363a8388b54b81ff87f9a776ad42fc03ca8e
Akshat Singhal <asinghal28@instgpu-01.cs.wisc.edu>
2025-10-25 17:06:07 -0500

    mlm

--- hw3/classifier.py
+++ hw3/classifier.py
@@ -5,13 +5,13 @@ import torch
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 from sklearn.metrics import classification_report, f1_score, recall_score, accuracy_score
-
+import json
 # change it with respect to the original model
 from tokenizer import BertTokenizer
 from bert import BertModel
 from optimizer import AdamW
 from tqdm import tqdm
-
+import pickle
 
 TQDM_DISABLE=True
 # fix the random seed
@@ -51,15 +51,82 @@ class BertSentClassifier(torch.nn.Module):
 
     def forward(self, input_ids, attention_mask):
         # todo
-        # the final bert contextualize embedding is the hidden state of [CLS] token (the first token)
-        ##AKS
         output=self.bert(input_ids, attention_mask)
-        # print(output.keys())
-        # print(output)
         cls_token = output['pooler_output']
-        # cls_token = cls_token[:, 0,]
         logits = self.classifier(cls_token)
         return F.log_softmax(logits, dim=1)
+
+
+class BertSentMLM(torch.nn.Module):
+    def __init__(self, config):
+        super(BertSentMLM, self).__init__()
+        # self.num_labels = config.num_labels
+        self.bert = BertModel.from_pretrained('bert-base-uncased')
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+        # pretrain mode does not require updating bert paramters.
+        for param in self.bert.parameters():
+            if config.option == 'pretrain':
+                param.requires_grad = False
+            elif config.option == 'finetune':
+                param.requires_grad = True
+
+        
+
+        self.mlm_classifier = torch.nn.Sequential(
+            torch.nn.Linear(config.hidden_size, config.hidden_size),
+            torch.nn.GELU(),
+            torch.nn.LayerNorm(config.hidden_size),
+            torch.nn.Linear(config.hidden_size, config.vocab_size)
+        )
+
+    def mask_input(self, input_ids, mlm_probability=0.15):
+
+
+        tokenizer=self.tokenizer
+        device = input_ids.device
+        labels = input_ids.clone()
+
+        probability_matrix = torch.full(labels.shape, mlm_probability, device=device)
+        special_tokens_mask = [
+            tokenizer.get_special_tokens_mask(val, already_has_special_tokens=True) 
+            for val in labels.tolist()
+        ]
+        special_tokens_mask = torch.tensor(special_tokens_mask, dtype=torch.bool, device=device)
+        probability_matrix.masked_fill_(torch.tensor(special_tokens_mask, dtype=torch.bool), value=0.0)
+        
+        masked_indices = torch.bernoulli(probability_matrix).bool()
+        labels[~masked_indices] = -100
+
+        # 80% MASK
+        indices_replaced = (torch.bernoulli(torch.full(labels.shape, 0.8, device=device)).bool() & masked_indices)
+        input_ids[indices_replaced] = tokenizer.convert_tokens_to_ids(tokenizer.mask_token)
+
+        # 10% random word
+        indices_random = (torch.bernoulli(torch.full(labels.shape, 0.5, device=device)).bool() 
+                        & masked_indices & ~indices_replaced)
+        random_words = torch.randint(len(tokenizer), labels.shape, dtype=torch.long, device=device)
+        input_ids[indices_random] = random_words[indices_random]
+
+        
+
+        return input_ids, labels
+
+    def forward(self, input_ids, attention_mask):
+        
+
+
+
+        input, labels=self.mask_input(input_ids)
+
+        output=self.bert(input, attention_mask)
+        
+        out_seq = output['last_hidden_state']
+        
+        prediction_scores = self.mlm_classifier(out_seq)
+
+        return prediction_scores, labels
+
 
 # create a custom Dataset Class to be used for the dataloader
 class BertDataset(Dataset):
@@ -107,6 +174,54 @@ class BertDataset(Dataset):
 
         return batches
 
+# create a custom Dataset Class to be used for the dataloader
+class BertDatasetMLM(Dataset):
+    def __init__(self, dataset, args):
+        self.dataset = dataset
+        self.p = args
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        ele = self.dataset[idx]
+        return ele
+
+    def pad_data(self, data):
+        sents = [x[0] for x in data]
+        # labels = [x[1] for x in data]
+        encoding = self.tokenizer(sents, return_tensors='pt', padding=True, truncation=True)
+        token_ids = torch.LongTensor(encoding['input_ids'])
+        attention_mask = torch.LongTensor(encoding['attention_mask'])
+        token_type_ids = torch.LongTensor(encoding['token_type_ids'])
+        # labels = torch.LongTensor(labels)
+
+        return token_ids, token_type_ids, attention_mask, sents
+
+    def collate_fn(self, all_data):
+        # print(all_data[0])
+        all_data.sort(key=lambda x: -len(x[1]))  # sort by number of tokens
+
+        batches = []
+        num_batches = int(np.ceil(len(all_data) / self.p.batch_size))
+
+        for i in range(num_batches):
+            start_idx = i * self.p.batch_size
+            data = all_data[start_idx: start_idx + self.p.batch_size]
+
+            token_ids, token_type_ids, attention_mask, sents = self.pad_data(data)
+            batches.append({
+                'token_ids': token_ids,
+                'token_type_ids': token_type_ids,
+                'attention_mask': attention_mask,
+                'sents': sents,
+            })
+
+        return batches
+
+
+
 
 # create the data which is a list of (sentence, label, token for the labels)
 def create_data(filename, flag='train'):
@@ -129,6 +244,43 @@ def create_data(filename, flag='train'):
         return data, len(num_labels)
     else:
         return data
+
+def create_data_mlm(filename, flag='train'):
+    # specify the tokenizer
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    # num_labels = {}
+    data = []
+
+    with open(filename, 'r') as fp:
+        for i, line in tqdm(enumerate(fp)):
+
+            if i>=100:
+                break
+
+            obj = json.loads(line)
+            sent = obj['reviewText'].strip().lower()
+            if not sent:
+                continue
+
+
+            # label, org_sent = line.split(' ||| ')
+            # sent = org_sent.lower().strip()
+            
+            # label = int(label.strip())
+            # if label not in num_labels:
+            #     num_labels[label] = len(num_labels)
+            data.append(sent)
+
+    data = [(sent, tokenizer.tokenize("[CLS] " + sent + " [SEP]")) for i in tqdm(data)]
+
+    with open("/u/a/s/asinghal28/private/NLP/Advanced-NLP/hw3/data/movie_reviews.pkl", 'wb') as f:
+        pickle.dump(data, f)
+
+
+    print(f"load {len(data)} data from {filename}")
+    
+    return data, tokenizer.vocab_size
+
 
 # perform model evaluation in terms of the accuracy and f1 score.
 def model_eval(dataloader, model, device):
@@ -172,7 +324,7 @@ def save_model(model, optimizer, args, config, filepath):
     print(f"save the model to {filepath}")
 
 def train(args):
-    device = torch.device('cuda') if args.use_gpu else torch.device('cpu')
+    device = torch.device('cuda:1') if args.use_gpu else torch.device('cpu')
     #### Load data
     # create the data and its corresponding datasets and dataloader
     train_data, num_labels = create_data(args.train, 'train')
@@ -221,6 +373,9 @@ def train(args):
             logits = model(b_ids, b_mask)
             loss = F.nll_loss(logits, b_labels.view(-1), reduction='sum') / args.batch_size
 
+
+            
+
             loss.backward()
             optimizer.step()
 
@@ -238,6 +393,98 @@ def train(args):
 
         print(f"epoch {epoch}: train loss :: {train_loss :.3f}, train acc :: {train_acc :.3f}, dev acc :: {dev_acc :.3f}")
 
+
+def train_mlm(args):
+    device = torch.device('cuda:2') if args.use_gpu else torch.device('cpu')
+    #### Load data
+
+    data, vocab_size = create_data_mlm(args.train, 'train')
+    dev_ratio=0.15
+
+    split_idx = int(len(data) * (1 - dev_ratio))
+    train_data = data[:split_idx]
+    dev_data = data[split_idx:]
+
+    train_dataset = BertDatasetMLM(train_data, args)
+    dev_dataset = BertDatasetMLM(dev_data, args)
+
+    train_dataloader = DataLoader(train_dataset, shuffle=True, batch_size=args.batch_size,
+                                  collate_fn=train_dataset.collate_fn)
+    dev_dataloader = DataLoader(dev_dataset, shuffle=False, batch_size=args.batch_size,
+                                collate_fn=dev_dataset.collate_fn)
+
+    #### Init model
+    config = {'hidden_dropout_prob': args.hidden_dropout_prob,
+              'hidden_size': 768,
+              'data_dir': '.',
+              'option': args.option,
+              'vocab_size': vocab_size}
+
+    config = SimpleNamespace(**config)
+
+    # initialize the Senetence Classification Model
+    model = BertSentMLM(config)
+    model = model.to(device)
+
+    lr = args.lr
+    ## specify the optimizer
+    optimizer = AdamW(model.parameters(), lr=lr)
+    best_dev_loss=100000000
+
+    ## run for the specified number of epochs
+    print("MLM Training started")
+    for epoch in range(args.epochs):
+        print(f"Epoch: {epoch}")
+        model.train()
+        train_loss = 0
+        num_batches = 0
+        for step, batch in enumerate(tqdm(train_dataloader, desc=f'train-{epoch}', disable=TQDM_DISABLE)):
+            b_ids, b_type_ids, b_mask, b_sents = batch[0]['token_ids'], batch[0]['token_type_ids'], batch[0][
+                'attention_mask'], batch[0]['sents']
+
+            b_ids = b_ids.to(device)
+            b_mask = b_mask.to(device)
+            # b_labels = b_labels.to(device)
+
+            optimizer.zero_grad()
+            
+            logits, labels = model(b_ids, b_mask)
+            
+            loss_fn = torch.nn.CrossEntropyLoss(ignore_index=-100)
+
+            loss = loss_fn(logits.view(-1, config.vocab_size), labels.view(-1))
+
+            loss.backward()
+            optimizer.step()
+
+            train_loss += loss.item()
+            num_batches += 1
+
+        train_loss = train_loss / (num_batches)
+
+        with torch.no_grad():
+            dev_loss = 0
+            num_dev_batches = 0
+            for step, batch in enumerate(tqdm(dev_dataloader, desc=f'train-{epoch}', disable=TQDM_DISABLE)):
+                
+                b_ids, b_type_ids, b_mask, b_sents = batch[0]['token_ids'], batch[0]['token_type_ids'], batch[0][
+                'attention_mask'], batch[0]['sents']
+
+                b_ids = b_ids.to(device)
+                b_mask = b_mask.to(device)
+                
+                logits, labels = model(b_ids, b_mask)
+                
+                loss = loss_fn(logits.view(-1, config.vocab_size), labels.view(-1))
+                dev_loss += loss.item()
+                num_dev_batches += 1
+        dev_loss /= num_dev_batches
+
+        if dev_loss < best_dev_loss:
+            best_dev_loss = dev_loss
+            save_model(model, optimizer, args, config, args.filepath)
+
+        print(f"epoch {epoch}: train loss :: {train_loss:.3f}, dev loss :: {dev_loss:.3f}")
 
 def test(args):
     with torch.no_grad():
@@ -279,17 +526,18 @@ def get_args():
     parser.add_argument("--epochs", type=int, default=10)
     parser.add_argument("--option", type=str,
                         help='pretrain: the BERT parameters are frozen; finetune: BERT parameters are updated',
-                        choices=('pretrain', 'finetune'), default="pretrain")
+                        choices=('pretrain', 'finetune'), default="finetune")
     parser.add_argument("--use_gpu", action='store_true', default=True )
     parser.add_argument("--dev_out", type=str, default="sst-dev-output.txt")
     parser.add_argument("--test_out", type=str, default="sst-test-output.txt")
     parser.add_argument("--filepath", type=str, default=None)
+    parser.add_argument("--mlm", type=str, default="False")
 
     # hyper parameters
     parser.add_argument("--batch_size", help='sst: 64, cfimdb: 8 can fit a 12GB GPU', type=int, default=64)
     parser.add_argument("--hidden_dropout_prob", type=float, default=0.3)
     parser.add_argument("--lr", type=float, help="learning rate, default lr for 'pretrain': 1e-3, 'finetune': 1e-5",
-                        default=1e-3)
+                        default=1e-5)
 
     args = parser.parse_args()
     print(f"args: {vars(args)}")
@@ -300,5 +548,11 @@ if __name__ == "__main__":
     if args.filepath is None:
         args.filepath = f'{args.option}-{args.epochs}-{args.lr}.pt' # save path
     seed_everything(args.seed)  # fix the seed for reproducibility
-    train(args)
-    test(args)
+
+    # prin
+
+    if args.mlm=="True":
+        train_mlm(args)
+    else:
+        train(args)
+        test(args)
